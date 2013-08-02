1693680eb65a890e3ee8c5945a6ce4e457df1892
jingyang <jingyang.tom@qq.com>
2013-08-02 17:37:55 +0800

    test in aptana

--- bookstore/apps/books/api_views.py
+++ bookstore/apps/books/api_views.py
@@ -5,15 +5,17 @@ Created on 2013-7-7
 @author: Tom
 '''
 from rest_framework import generics, status
-from books.serializers import BookSerializer
+from books.serializers import BookSerializer, BasicBookSerializer
 from books.models import Book
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 class BookList(generics.ListAPIView):
     '''list all the books'''
+    
     model = Book
-    serializer_class = BookSerializer
+#     serializer_class = BookSerializer
+    serializer_class = BasicBookSerializer
     
 class BookDetail(GenericAPIView):
     '''detail of a book'''
--- bookstore/apps/books/serializers.py
+++ bookstore/apps/books/serializers.py
@@ -11,39 +11,43 @@ from rest_framework import serializers
 from books.models import Book, Author
 from rest_framework.fields import Field
 from django.conf import settings
-from common.utils import CustomSerializerMethodField, strTime
 
-#class AuthorSerializer(serializers.ModelSerializer):
-#    
-#    class Meta:
-#        model = Author
+class AuthorSerializer(serializers.ModelSerializer):
+     
+    class Meta:
+        model = Author
+#         fields = ('id', 'name')
 
 
 class BookSerializer(serializers.ModelSerializer):
     
-#    author = AuthorSerializer()
+    author = AuthorSerializer()
     cover = serializers.SerializerMethodField('getCoverPath')
     boughtCount = Field(source='bought_count')
     category = serializers.SlugRelatedField(slug_field='label')
     publishDate = Field(source='publish_date')
-    regDate = CustomSerializerMethodField('strTime', 'reg_date')
+    #regDate = serializers.SerializerMethodField('strRegDate')
+    regDate = Field(source='reg_date')
     commentCount = Field(source='comment_count')
     
     class Meta:
         model = Book
-#        fields = ('id', 'name')
         fields = ('id', 'name', 'author', 'price', 'isbn', 'press', 'desc', 'binding', 
             'pages', 'cover', 'boughtCount', 'category', 'stock', 'publishDate', 
             'regDate', 'commentCount')
-    
+#         depth = 1  # 指定关联对象显示的深度
+        
     def getCoverPath(self, obj):
         return settings.MEDIA_URL + 'img/' + obj.mpic
+     
+    def strRegDate(self, obj):
+        return obj.reg_date.strftime('%Y-%m-%d %H:%M:%S')
+        
+class BasicBookSerializer(BookSerializer):
     
-    def strTime(self, obj, attr):
-        return strTime(getattr(obj, attr))
-        
-        
-        
+    class Meta:
+        model = Book
+        fields = ('id', 'name', 'cover')       
         
         
 
